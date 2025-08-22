c6004ad1e8b69cc5ae13c41e8e4dafc718a0dafc
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2025-08-22 15:59:59 +0530

    Feat: Add List group item

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,14 +1,45 @@
-import { Footer } from "../component/Footer"
-import { Header } from "../component/Header"
+import { Footer } from "../component/Footer";
+import { Header } from "../component/Header";
+
+const ItemsDetails = ({ cart }) => {
+  const listedItem = cart.map(({ id, name, price, quantity }) => {
+    return (
+      <li
+        key={id}
+        className='list-group-item d-flex justify-content-between align-items-start'>
+        <div className=''>
+          <h5>{name}</h5>
+          <p className='fw-lighter'>Price: {price}</p>
+          <br />
+          <p>Quantity: {quantity}</p>
+        </div>
+        <span className=' text-bg-primary badge rounded'>
+          Total Price: {(price * quantity).toFixed(2)}
+        </span>
+      </li>
+    );
+  });
+  return listedItem;
+};
 
 export const Cart = () => {
-    return (
-        <>
-        <Header/>
-        <main className="container-md">
-        <h1>Cart Js</h1>
-        </main>
-        <Footer/>
-        </>
-    )
-}
+  const cart = [
+    { id: 1, name: "Product 1", price: 99.99, quantity: 2 },
+    { id: 2, name: "Product 2", price: 129.99, quantity: 1 },
+    { id: 3, name: "Product 3", price: 79.99, quantity: 3 },
+  ];
+  return (
+    <>
+      <Header />
+      <main className='container'>
+        <h3 className='display-3'>Shopping Cart</h3>
+        <ul className='list-group'>
+          <section className="py-3">
+            <ItemsDetails cart={cart} />
+          </section>
+        </ul>
+      </main>
+      <Footer />
+    </>
+  );
+};
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,86 +1,43 @@
 import { Link } from "react-router-dom"
 import { Footer } from "../component/Footer"
 import { Header } from "../component/Header"
-
+import products from "../data/products"
 const ProductListings = ( { data } ) => {
-
-    const resultDisplay = data.map( ( { id, name, description, price, productImgUrl } ) => {
-        return (
-            <>
-                <div className="card">
-                    <div className="row">
-                        <div className="col-md-4 col-12">
-                            <img src={ productImgUrl } alt="placehold" className="img-fluid roundeds-start" />
-                        </div>
-                        <div className="col-md-8 col-12">
-                            <div className="card-body">
-                                <h5 className="fw-semibold">{ name }</h5>
-                                <p className="card-text">{ description }</p>
-                                <p>Price: { price }</p>
-                                <Link to={ `/product/p/:${ id }` }></Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </>
-        )
-    })
-return resultDisplay;
+  const resultDisplay = data.map( ( { id, name, description, price, productImgUrl } ) => {
+    return (
+      <>
+        <div className="card w-75 m-4">
+          <div className="row">
+            <div className="col-md-4 col-12">
+              <img src="https://placehold.co/450" alt="placehold" className="img-fluid rounded-start" />
+            </div>
+            <div className="col-md-8 col-12">
+              <div className="card-body">
+                <h5 className="fw-semibold">{ name }</h5>
+                <p className="card-text">{ description }</p>
+                <p>Price: { price }</p>
+                <Link to={ `/product/${ id }` } className="btn btn-primary">View Details</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  } )
+  return resultDisplay;
 }
 
 export const Product = () => {
-        const products = [
-    {
-      id: "1",
-      name: "Product 1",
-      description:
-        "This is Description of Product 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      price: 99.99,
-      availableColors: ["Red", "Blue", "Green"],
-      isReturnApplicable: true,
-      productImageUrl: "https://placehold.co/300x300/png",
-    },
-    {
-      id: "2",
-      name: "Product 2",
-      description:
-        "This is Description of Product 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      price: 129.99,
-      availableColors: ["White", "Black"],
-      isReturnApplicable: true,
-      productImageUrl: "https://placehold.co/300x300/png",
-    },
-    {
-      id: "3",
-      name: "Product 3",
-      description:
-        "This is Description of Product 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      price: 79.99,
-      availableColors: ["Yellow"],
-      isReturnApplicable: false,
-      productImageUrl: "https://placehold.co/300x300/png",
-    },
-    {
-      id: "4",
-      name: "Product 4",
-      description:
-        "This is Description of Product 4. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      price: 49.99,
-      availableColors: ["Purple", "Yellow"],
-      isReturnApplicable: true,
-      productImageUrl: "https://placehold.co/300x300/png",
-    },
-  ];
-    return (
-        <>
-            <Header />
-            <main className="container">
-                <h1>Product Js</h1>
-                <section>
-                    <ProductListings data={products} />
-                </section>
-            </main>
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <section>
+          <h1 className="fw-normal">Product Js</h1>
+          <ProductListings data={ products } />
+        </section>
+      </main>
+      <Footer />
+    </>
+  )
 }
