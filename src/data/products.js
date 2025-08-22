c77b6edca32fc7979ed1ce5c3cccf5886e1cfac3
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2025-08-22 13:07:42 +0530

    Feat : fix card

---
+++ src/data/products.js
@@ -0,0 +1,43 @@
+    const products = [
+        {
+            id: 1,
+            name: "Product 1",
+            description:
+                "This is Description of Product 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            price: "$99.99",
+            availableColors: ["Red", "Blue", "Green"],
+            isReturnApplicable: true,
+            productImageUrl: "https://placehold.co/300x200/png",
+        },
+        {
+            id: 2,
+            name: "Product 2",
+            description:
+                "This is Description of Product 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            price: "$129.99",
+            availableColors: ["White", "Black"],
+            isReturnApplicable: true,
+            productImageUrl: "https://placehold.co/300x200/png",
+        },
+        {
+            id: 3,
+            name: "Product 3",
+            description:
+                "This is Description of Product 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            price: "$79.99",
+            availableColors: ["Yellow"],
+            isReturnApplicable: false,
+            productImageUrl: "https://placehold.co/300x200/png",
+        },
+        {
+            id: 4,
+            name: "Product 4",
+            description:
+                "This is Description of Product 4. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            price: "$49.99",
+            availableColors: ["Purple", "Yellow"],
+            isReturnApplicable: true,
+            productImageUrl: "https://placehold.co/300x200/png",
+        },
+    ];
+    export default products;
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,51 +1,9 @@
 import { useParams } from "react-router-dom";
 import { Footer } from "../component/Footer"
 import { Header } from "../component/Header"
-
+import products from "../data/products";
 export const ProductDetails = () => {
     const { productId } = useParams();
-    const products = [
-        {
-            id: 1,
-            name: "Product 1",
-            description:
-                "This is Description of Product 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            price: "$99.99",
-            availableColors: ["Red", "Blue", "Green"],
-            isReturnApplicable: true,
-            productImageUrl: "https://placehold.co/300x200/png",
-        },
-        {
-            id: 2,
-            name: "Product 2",
-            description:
-                "This is Description of Product 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            price: "$129.99",
-            availableColors: ["White", "Black"],
-            isReturnApplicable: true,
-            productImageUrl: "https://placehold.co/300x200/png",
-        },
-        {
-            id: 3,
-            name: "Product 3",
-            description:
-                "This is Description of Product 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            price: "$79.99",
-            availableColors: ["Yellow"],
-            isReturnApplicable: false,
-            productImageUrl: "https://placehold.co/300x200/png",
-        },
-        {
-            id: 4,
-            name: "Product 4",
-            description:
-                "This is Description of Product 4. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            price: "$49.99",
-            availableColors: ["Purple", "Yellow"],
-            isReturnApplicable: true,
-            productImageUrl: "https://placehold.co/300x200/png",
-        },
-    ];
     const { id, name, description, price, availableColors, isReturnApplicable, productImageUrl } = products.find( ( { id } ) => id === Number( productId ) )
     return (
         <>
